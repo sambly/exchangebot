e948c32c79471a9ba682ac4f217c3e76e98df9a8
Kudrjavcev.EA <sambly>
2024-12-02 11:42:30 +0300

    add sourcemap vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,22 +1,23 @@
 import { defineConfig } from 'vite';
 import path from 'path';
 
-export default defineConfig({
-  root: path.resolve(__dirname), 
-  base:"/trade/",
+export default defineConfig(({ mode }) => ({
+  root: path.resolve(__dirname),
+  base: "/trade/",
   build: {
     outDir: path.resolve(__dirname, 'dist'),
     rollupOptions: {
-      input: path.resolve(__dirname, 'index.html'), 
-    }
+      input: path.resolve(__dirname, 'index.html'),
+    },
+    sourcemap: mode === 'development', // Используем mode для включения sourcemap
   },
   resolve: {
     alias: {
       '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-    }
+    },
   },
   server: {
     port: 8080,
-    hot: true
-  }
-});
+    hot: true,
+  },
+}));
